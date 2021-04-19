0c56e2b62564f89f6b9d67cbd0bbe9f3b69e9411
Augusto Lima <augustolimads@gmail.com>
2021-04-19 16:48:07 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Welcome />
     </View>
   );
 }
